6f081d84fe53773be017d5c075c729247e1ba364
Gabriel Sandoval <gabrielsandoval1020@gmail.com>
2024-01-10 02:43:57 +0800

    Add Rubocop

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,5 +1,4 @@
 class BoardsController < ApplicationController
-
   def index
     respond_to do |format|
       format.html do # index.html.erb
--- app/models/board.rb
+++ app/models/board.rb
@@ -4,28 +4,28 @@ class Board < ApplicationRecord
   MAX_ATTEMPTS = 1000
 
   def place_mines!
-    raise ArgumentError if width == 0 || height == 0
+    raise ArgumentError if width.zero? || height.zero?
 
-    placements = []  # [[1,2]] | [[3,4]]
+    placements = [] # [[1,2]] | [[3,4]]
 
     num_mines.times do
       attempts = 0
       prev_count = placements.size
 
       while prev_count == placements.size
-        raise StandardError, "Unable to place mines" if attempts > MAX_ATTEMPTS
+        raise ArgumentError, "Unable to place mines" if attempts > MAX_ATTEMPTS
 
         row = rand(height)
         col = rand(width)
-        placements |= [[row,col]]
+        placements |= [[row, col]]
 
         attempts += 1
       end
     end
 
     locations = placements.map { |loc| loc.join(",") }.join("|") # r,c|r,c|r,c
-    update(locations: locations)
-  rescue StandardError, ArgumentError
+    update(locations:)
+  rescue ArgumentError
     update(locations: nil)
   end
 end
